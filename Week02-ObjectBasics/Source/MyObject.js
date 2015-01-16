de10f5fca2916dd1b9c26dc510a463baabe15916
Cameron Cooks <cameronc56@ymail.com>
2015-01-15 17:00:08 -0800

    added .gitignore

--- Week02-ObjectBasics/Source/MyObject.js
+++ Week02-ObjectBasics/Source/MyObject.js
@@ -5,17 +5,17 @@ exports.myObject;
 var myObject = {
 	
 	height: function() {
-		"use strict";
+		"use strict";,
 		return "5 feet 4 inches";
 	},
 
 	weight: function() {
-		"use strict";
+		"use strict";,
 		return "120 pounds";
 	},
 
 	eyeColor: function() {
-		"use strict";
+		"use strict";,
 		return "green";
 	},
 
@@ -26,11 +26,4 @@ var myObject = {
 	hello: "Hello World"
 
 };
-
-
-
-
-
-
-
 
