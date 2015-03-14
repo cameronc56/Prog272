30278e011dced3c5f1255dc8d01a1a2b968daafe
Cameron Cooks <cameronc56@ymail.com>
2015-03-14 16:21:18 -0700

    added week 10

---
+++ Week10-Callbacks/public/javascripts/Callbacks.js
@@ -0,0 +1,32 @@
+function callbackHandler(int1, int2, callback) {
+		var message = callback(int1, int2).toString();        
+		showMessage(message);
+		console.log("hello");
+}
+
+function showMessage(message) {
+				$("#myList").append("<li>" + message + "</li>");
+				console.log(message);
+};
+
+function callRoute() {
+		        showMessage("callRoute was called");
+				        $.getJSON("/getNine", function(json) {
+								                showMessage(JSON.stringify(json));
+												                showMessage(json.result);
+																        });
+};
+
+
+callbackHandler(3, 3, function(a, b) {
+		        return a + b;
+});
+
+callbackHandler(4, 2, function(a, b) {
+		        return a % b;
+});
+
+callbackHandler(4, 4, function(a, b) {
+		        return b * 5280;
+});
+
